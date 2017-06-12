5c1b98467ceeb46217f2a493f735a778d215782f
ja0ck5 <975117619@qq.com>
2017-06-12 22:47:14 +0800

    update annotations

--- spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/App.java
+++ spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/App.java
@@ -2,9 +2,10 @@ package com.ja0ck5.discovery;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class App {
 	public static void main(String[] args) {
 
