79db639c6d0ef81f70b20fdfdd8c14c6661c35c6
ja0ck5 <975117619@qq.com>
2017-06-12 22:41:29 +0800

    update eureka service

--- spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/App.java
+++ spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/App.java
@@ -4,6 +4,7 @@ package com.ja0ck5.discovery;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class App {
 	public static void main(String[] args) {
 
