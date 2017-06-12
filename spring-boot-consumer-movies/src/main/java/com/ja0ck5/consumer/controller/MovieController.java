f22cf82a1988d7755759815205dc0e6c6f616c7f
ja0ck5 <975117619@qq.com>
2017-06-12 08:06:52 +0800

    update microservice to parent module

--- spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/controller/MovieController.java
+++ spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.ja0ck5.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,17 @@ import com.ja0ck5.consumer.entity.User;
 @RestController
 public class MovieController {
 
+	@Value("${user.userService.path}")
+	String userServicePath;
+	
 	@Autowired
 	RestTemplate restTemplate;
 
+	
+	
 	@GetMapping("/user/{id}")
 	public User findById(@PathVariable Long id) {
-		return this.restTemplate.getForObject("http://localhost:8080/simple/" + id, User.class);
+		return this.restTemplate.getForObject(this.userServicePath + id, User.class);
 	}
 
 }
