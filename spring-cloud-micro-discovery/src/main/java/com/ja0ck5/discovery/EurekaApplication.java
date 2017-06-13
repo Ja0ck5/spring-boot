5f13756a9c000e63ac7ed79126d245c222604756
ja0ck5 <975117619@qq.com>
2017-06-13 08:22:41 +0800

    update multi datasource name

--- spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/EurekaApplication.java
+++ spring-cloud-micro-discovery/src/main/java/com/ja0ck5/discovery/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.ja0ck5.discovery;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
