9091cbce15aa9a40dc458c0b25da00dd606c8440
ja0ck5 <975117619@qq.com>
2017-07-13 00:10:05 +0800

    use ribbon

--- spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/controller/MovieController.java
+++ spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.ja0ck5.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,27 @@ import com.ja0ck5.consumer.entity.User;
 @RestController
 public class MovieController {
 
-	@Value("${user.userService.path}")
-	String userServicePath;
-	
-	@Autowired
-	RestTemplate restTemplate;
+    @Value("${user.userService.path}")
+    String userServicePath;
 
-	
-	
-	@GetMapping("/user/{id}")
-	public User findById(@PathVariable Long id) {
-		return this.restTemplate.getForObject(this.userServicePath + id, User.class);
-	}
+    @Autowired
+    @LoadBalanced // 使用 ribbon. 具有 ribbon 负载均衡
+    RestTemplate restTemplate;
+
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+    }
+
+    /**
+     * use ribbon
+     * @param id
+     * @return
+     */
+    @GetMapping("/user/ribbon/{id}")
+    public User useRibbon(@PathVariable Long id) {
+        return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+    }
 
 }
