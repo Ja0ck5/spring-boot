bcb97d8f17f58db6ad2829b083277ec32ad8139b
ja0ck5 <975117619@qq.com>
2017-07-12 23:51:37 +0800

    eureka server and client

--- spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/SpringBootConsumerMoviesApplication.java
+++ spring-boot-consumer-movies/src/main/java/com/ja0ck5/consumer/SpringBootConsumerMoviesApplication.java
@@ -3,11 +3,13 @@ package com.ja0ck5.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootConsumerMoviesApplication {
 
 	/**
--- spring-boot-provider-user/src/main/java/com/ja0ck5/provider/controlller/UserController.java
+++ spring-boot-provider-user/src/main/java/com/ja0ck5/provider/controlller/UserController.java
@@ -1,6 +1,11 @@
 package com.ja0ck5.provider.controlller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +16,24 @@ import com.ja0ck5.provider.entity.User;
 @RestController
 public class UserController {
 
+	@Qualifier("eurekaClient")
+	@Autowired
+	EurekaClient eurekaClient;
+
+	@Autowired
+	DiscoveryClient discoveryClient;
+
 	@Autowired
 	UserDao userDao;
 	
 	@GetMapping("/simple/{id}")
 	public User findById(@PathVariable Long id){
 		return userDao.findOne(id);
+	}
+
+	@GetMapping("/eureka/client/{id}")
+	public String getEurekaClientInfo(@PathVariable Long id){
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("MICRO-USER", false);
+		return instanceInfo.getHomePageUrl();
 	}
 }
